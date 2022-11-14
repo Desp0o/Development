4d2da9349a11436d0ac5e3b931c2f932feef68b1
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2022-11-14 07:43:26 -0800

    added dashboard but need some fix

--- src/main_GEO.js
+++ src/main_GEO.js
@@ -1,15 +1,39 @@
-import React from "react";
-import {useRef} from "react";
-import {link} from "react-router-dom";
 
-import Navbar from "./navbarGEO";
+import {React, useEffect, useState,useRef} from "react";
+import {Link, link} from "react-router-dom";
+
+import logo from './images/logo.png'
+import {AiOutlineMenu}  from "react-icons/ai"
+import {AiOutlineClose} from "react-icons/ai"
+
 import Slider from './Slider'
 
 import './App.css'
 import './homeGeo.css'
 import './font.css'
+import './navbar.css'
+import './font.css'
 
 function MainGeo(){
+    const [toggle, setToggle]   = useState(false)
+    const [open, setOpen] = useState('burger_logo')
+    const [close, setClose] = useState('hide_logo')
+    const [dashboard, setDashboard] = useState('hidden_dashboard')
+    
+    
+    function burgerToggle(){
+        if(!toggle){
+            setOpen('hide_logo')
+            setClose('burger_logo-close')
+            setToggle(true)
+            setDashboard('dashboard')
+        }else{
+            setOpen('burger_logo')
+            setClose('hide_logo')
+            setToggle(false)
+            setDashboard('dashboard hidden_dashboard')
+        }
+    }
 
     const headerRef = useRef(null)
 
@@ -19,14 +43,68 @@ function MainGeo(){
 
     return(
         <div >
-            <Navbar/>
+        
+        {/* ნავიგაცია ///////////////////// */}
+        <div className="Nav">
+            <div className="navbar-upper">
+                <p><span>მისამართი /</span>გულუას ქ. 10/12, თბილისი</p>
+                <p><span>ტელეფონი /</span>+995 596 33 33 00</p>
+                <p><span>ელ-ფოსტა /</span>sales@allianceconstruction.ge</p>
+            </div>
 
-            <div className="carousel_main_page">
+            <div className="navbar-bottom">
+                <div className="logo_div">
+                    <img src={logo} />
+                </div>
+
+                <div className="navbar_menu">
+                    <span>მთავარი</span>
+                    <span>ჩვენ შესახებ</span>
+                    <span>პროექტის შესახებ</span>
+                    <Link><span>ბინები</span></Link>
+                    <span>კონტაქტი</span>
+                </div>
+
+                <div className="language">
+                    <Link>ინგლისური</Link>
+                    <Link>რუსული</Link>
+                </div>
+            </div>
+
+
+            <div className="burger_menu">
+                <div className="burger_menu_compay_logo">
+                    <img src={logo}/>
+                </div>
+
+                <div className="burger-menu_logo">
+                    <AiOutlineMenu className={open} onClick={burgerToggle}/>
+                    <AiOutlineClose className={close} onClick={burgerToggle}/>
+                </div>
+            </div>
+
+            <div className={dashboard}>
+                <div className="dashboard_inner">
+                    <span>მთავარი</span>
+                    <span>ჩვენ შესახებ</span>
+                    <span>პროექტის შესახებ</span>
+                    <Link><span>ბინები</span></Link>
+                    <span>კონტაქტი</span>
+                    <Link>ინგლისური</Link>
+                    <Link>რუსული</Link>
+                </div>
+            </div>
+        </div>
+
+        
+
+        {/* სლაიდერი ///////////////////  */}
+        <div className="carousel_main_page">
             <Slider/>
             <div className="call_btn" onClick={()=>handleClick(headerRef)}>
-                <p style={{color:'white'}}>მოითხოვე ზარი</p>
+                    <p style={{color:'white'}}>მოითხოვე ზარი</p>
             </div>
-            </div>
+         </div>
             
 
             
