8775291c58961a37b495cf0988d601c821b1600d
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2022-11-17 05:22:35 -0800

    finished WhyTwins page with animations

--- src/whyTwins/WhyTwins.js
+++ src/whyTwins/WhyTwins.js
@@ -3,38 +3,68 @@ import Fade from 'react-reveal/Fade';
 
 import './WhyTwins.css'
 
-import '../images/tree.png'
-import '../images/bellboy.png'
-import '../images/secure.png'
-import '../images/playground.png'
-import '../images/parking.png'
-import '../images/football-field.png'
+import tree from'../images/tree.png'
+import bellboy from'../images/bellboy.png'
+import secure from'../images/secure.png'
+import playground from '../images/playground.png'
+import parking from'../images/parking.png'
+import stadium from'../images/football-field.png'
+import whyTwinBG from '../images/whyTwins.png'
 
 
 function WhyTwins() {
     return(
         <div className="whyTwins">
+
+            <Fade delay={200}><p className="whyTwins_header">რატომ Twin Development?</p></Fade>
+
             {/* შიდა მხარე */}
             <div className="whyTwins_inner">
 
                 {/*მარცხენა მხარე*/}
                 <div className="whyTwins_inner_left">
                     <div className="row_for_whyTwins">
-                        <div className="icon_n_txt"></div>
-                        <div className="icon_n_txt"></div>
-                        <div className="icon_n_txt"></div>
+                       <Fade bottom><div className="icon_n_txt">
+                            <img className="whyTwins_ico" src={playground} />
+                            <p>სათამაშო მოედანი</p>
+                        </div></Fade> 
+
+                        <Fade bottom delay={100}><div className="icon_n_txt">
+                            <img className="whyTwins_ico" src={stadium} />
+                            <p>სპორტული მოედანი</p>
+                        </div></Fade>
+
+                        <Fade bottom delay={200}><div className="icon_n_txt">
+                            <img className="whyTwins_ico" src={tree} />
+                            <p>გამწვანება</p>
+                        </div></Fade>
+
                     </div>
 
+                    {/* მეორე ხაზი იქონების */}
                     <div className="row_for_whyTwins">
-                        <div className="icon_n_txt"></div>
-                        <div className="icon_n_txt"></div>
-                        <div className="icon_n_txt"></div>
+                        
+                        <Fade bottom delay={220}><div className="icon_n_txt">
+                            <img className="whyTwins_ico" src={parking} />
+                            <p>შიდა და გარე პარკინგი</p>
+                        </div></Fade>
+
+                        <Fade bottom delay={240}><div className="icon_n_txt">
+                            <img className="whyTwins_ico" src={bellboy} />
+                            <p>კონსიერჟი</p>
+                        </div></Fade>
+
+                        <Fade bottom delay={260}><div className="icon_n_txt">
+                            <img className="whyTwins_ico" src={secure} />
+                            <p>დაცვის სისტემა</p>
+                        </div></Fade>
+
                     </div>
                 </div>
 
                 {/* მარჯვენა მხარე */}
                 <div className="whyTwins_inner_right">
-
+                    <Fade right><img className="whyTwins_inner_BG" src={whyTwinBG} /></Fade>
                 </div>
 
             </div>
