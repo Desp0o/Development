f591e610624be34f005078a63dca50e3ea2f66d8
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2022-11-17 03:21:35 -0800

    changed selling home Font Weight

--- src/whyTwins/WhyTwins.js
+++ src/whyTwins/WhyTwins.js
@@ -2,9 +2,24 @@ import {React, useEffect, useState,useRef} from "react";
 import Fade from 'react-reveal/Fade';
 
 
+
 function WhyTwins() {
     return(
-        <div>
+        <div className="whyTwins">
+            {/* შიდა მხარე */}
+            <div className="whyTwins_inner">
+
+                {/*მარცხენა მხარე*/}
+                <div className="whyTwins_inner_left">
+a
+                </div>
+
+                {/* მარჯვენა მხარე */}
+                <div className="whyTwins_inner_right">
+
+                </div>
+
+            </div>
 
         </div>
     )
